5ed663b9d498cb7338cf78098caf2e26272a95a1
abdullahbadar2009-cmyk <abdullahbadar2009@gmail.com>
2025-12-11 00:36:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+let arr=[2, 8, 5, 12]
+let count=0
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>5){
+        count++
+        
+    }
+}
+console.log(count)
+
+let ar=[1, 4, 6, 7, 10]
+
+let coun=0
+for(let i=0;i<ar.length;i++){
+    if(ar[i] %2===0){
+        coun++
+    }
+}
+console.log(coun)
+
+let array=["Omar", "Ali", "Sara", "Zaid", "Mo"]
+
+let cou=0
+for(let i=0;i<array.length;i++){
+    if(array[i].length===4){
+        cou++
+    }
+}
+console.log(cou)
+
+
+ary=[5, 1, 2, 8, 0]
+
+let co=0
+for(let i=0;i<ary.length;i++){
+    if(ary[i]<3){
+        co++
+    }
+}
+
+console.log(co)
